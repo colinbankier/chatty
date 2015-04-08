0fc127e24c74cea2d124625f512ef8028830d1db
colinbankier <colinbankier@gmail.com>
2015-04-08 23:03:55 +1000

    Message sending and displaying.

--- web/channels/rooms.ex
+++ web/channels/rooms.ex
@@ -6,4 +6,10 @@ defmodule Chatty.Channels.Rooms do
     broadcast socket, "user:entered", %{username: message["username"] || "anon"}
     {:ok, socket}
   end
+
+  def handle_in(topic = "new:message", message, socket) do
+    IO.puts "Handle #{topic}"
+    broadcast! socket, topic, message
+    {:ok, socket}
+  end
 end
