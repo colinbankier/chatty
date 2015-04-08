2886c3d30c69d2b26aa2439c9596ff969dc8ae33
colinbankier <colinbankier@gmail.com>
2015-04-08 17:38:18 +1000

    Display user entered message on page.

--- web/channels/rooms.ex
+++ web/channels/rooms.ex
@@ -2,7 +2,8 @@ defmodule Chatty.Channels.Rooms do
   use Phoenix.Channel
 
   def join(topic, message, socket) do
-    IO.puts "JOIN: #{socket.channel}:#{topic}:#{message}"
+    IO.puts "JOIN: #{socket.channel}:#{topic}:#{inspect message}"
+    broadcast socket, "user:entered", %{username: message["username"] || "anon"}
     {:ok, socket}
   end
 end
