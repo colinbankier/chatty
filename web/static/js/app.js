fcbd039cd07ea10ebec03ec62815611864338d86
colinbankier <colinbankier@gmail.com>
2015-04-09 12:37:36 +1000

    Move js out of template file.

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -4,6 +4,34 @@ import {Socket} from "phoenix"
 // socket.join("topic:subtopic", {}, chan => {
 // })
 
+$(function(){
+  var socket = new Phoenix.Socket("ws://" + location.host + "/ws");
+  var $messages = $("#messages");
+  var $messageInput = $("#message-input");
+  var $usernameInput = $("#username");
+
+  socket.join("rooms", {}, function(channel){
+    channel.on("user:entered", function(message){
+      $messages.append("<br/>[" + message.username + "] entered");
+    });
+
+    channel.on("new:message", function(message){
+      $messages.append("<br/>[" + message.username + "] " + message.content);
+    });
+
+    $messageInput.off("keypress").on("keypress", function(e){
+      console.log("pressed");
+      if (e.keyCode ==13) {
+        channel.send("new:message", {
+          content: $messageInput.val(),
+          username: $usernameInput.val(),
+        });
+        $messageInput.val("");
+      }
+    });
+  });
+});
+
 let App = {
 }
 
