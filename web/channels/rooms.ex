8233815f577ea61b69dd794c6fbe739789dd95a8
colinbankier <colinbankier@gmail.com>
2015-04-10 13:52:49 +1000

    Use logger instead of puts.

--- web/channels/rooms.ex
+++ web/channels/rooms.ex
@@ -1,14 +1,15 @@
 defmodule Chatty.Channels.Rooms do
   use Phoenix.Channel
+  require Logger
 
   def join(topic, message, socket) do
-    IO.puts "JOIN: #{socket.channel}:#{topic}:#{inspect message}"
+    Logger.debug "JOIN: #{socket.channel}:#{topic}:#{inspect message}"
     broadcast socket, "user:entered", %{username: message["username"] || "anon"}
     {:ok, socket}
   end
 
   def handle_in(topic = "new:message", message, socket) do
-    IO.puts "Handle #{topic}"
+    Logger.debug "Handle #{topic}"
     broadcast! socket, topic, message
     {:ok, socket}
   end
