0d60922638acda7c9f604c0f7e08473da1a9cf62
colinbankier <colinbankier@gmail.com>
2015-04-10 17:31:36 +1000

    Add socket to a room.

--- lib/room_server.ex
+++ lib/room_server.ex
@@ -9,19 +9,39 @@ defmodule Chatty.RoomServer do
     GenServer.call(:room_server, {:rooms})
   end
 
+  def join_room socket, room do
+    GenServer.cast(:room_server, {:join_room, socket, room})
+  end
+
+  def room_members room do
+    GenServer.call(:room_server, {:room_members, room})
+  end
+
   def start_link do
     GenServer.start_link(__MODULE__, nil, name: :room_server)
   end
 
   def init(_) do
-    {:ok, []}
+    {:ok, %{}}
   end
 
   def handle_cast({:add_room, room}, rooms) do
-    {:noreply, [ room | rooms ]}
+    {:noreply, Dict.put_new(rooms, room, HashSet.new)}
+  end
+
+  def handle_cast({:join_room, socket, room}, rooms) do
+      rooms = Dict.update(rooms, room, HashSet.new, fn members ->
+        Set.put(members, socket)
+      end)
+      {:noreply, rooms}
   end
 
   def handle_call({:rooms}, _, rooms) do
-    {:reply, rooms, rooms}
+    {:reply, Dict.keys(rooms), rooms}
+  end
+
+  def handle_call({:room_members, room}, _, rooms) do
+    members = Dict.get(rooms, room) |> Set.to_list
+    {:reply, members, rooms}
   end
 end
--- test/room_server_test.exs
+++ test/room_server_test.exs
@@ -7,4 +7,13 @@ defmodule RoomServerTest do
 
     assert Enum.member? RoomServer.rooms, "my_room"
   end
+
+  test "Can join a socket to a room" do
+    socket = %{socket: 1234}
+    RoomServer.add_room "my_room"
+    RoomServer.join_room socket, "my_room"
+
+    sockets_in_room = RoomServer.room_members "my_room"
+    assert Enum.member? sockets_in_room, socket
+  end
 end
