7c98bd076961bed03a9d86005fab7e50aab9d206
colinbankier <colinbankier@gmail.com>
2015-04-22 17:24:30 +1000

    Add user:entered broadcast.

--- web/channels/chat.ex
+++ web/channels/chat.ex
@@ -4,7 +4,15 @@ defmodule Chatty.ChatChannel do
 
   def join(topic, message, socket) do
     Logger.debug "JOIN: #{socket.channel}:#{topic}:#{inspect message}"
+    send(self, {:after_join, message})
     {:ok, socket}
+  end
+
+  def handle_info({:after_join, message}, socket) do
+    broadcast! socket, "user:entered", %{
+      username: message["username"] || "anonymous"
+    }
+    {:noreply, socket}
   end
 end
 
--- web/static/js/app.js
+++ web/static/js/app.js
@@ -2,8 +2,14 @@ import {Socket} from "phoenix"
 
 $(function(){
   var socket = new Phoenix.Socket("/ws");
+  var $messages = $("#messages");
+
   socket.connect();
-  socket.join("chat", {}, function(channel){
+  socket.join("chat", {})
+    .receive("ok", function(channel){
+    channel.on("user:entered", function(message){
+      $messages.append("<br/>[" + message.username + "] entered");
+    });
   });
 });
 
